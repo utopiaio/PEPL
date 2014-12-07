6c28c228f128e43ddb74b6d92ea8762daadb81b3
Moe Szyslak <moe.duffdude@gmail.com>
2014-12-07 08:02:16 +0300

    login, sign-up good
    - no tracking ;)

---
+++ public/assets/js/app.js
@@ -0,0 +1,85 @@
+var app = angular.module('pepl', ['ngRoute', 'ngAnimate', 'ngTouch', 'ngMaterial', 'ngAria', 'moeAuth', 'moeProgressMaterial']);
+
+
+
+app.controller('appController', ['$rootScope', '$http', '$location', '$mdSidenav', 'Auth', 'xhrInProgress', function ($rootScope, $http, $location, $mdSidenav, Auth, xhrInProgress) {
+  xhrInProgress.listenToXHR();
+
+  $rootScope.showFab = true;
+
+  $rootScope.$on('$routeChangeError', function (event, current, previous, rejection) {
+    console.error(rejection);
+  });
+
+  $rootScope.$on('$routeChangeStart', function (event, target) {
+  });
+
+  $rootScope.$on('$routeChangeSuccess', function (event, target) {
+    $rootScope.showFab = (target.$$route.originalPath !== '/login');
+  });
+
+  this.toggleMenu = function() {
+    $mdSidenav('menu').toggle();
+  };
+
+  this.closeMenu = function () {
+    $mdSidenav('menu').close();
+  };
+
+  this.activateMenu = function (e, url) {
+    $('md-list.main-menu md-item button').removeClass('active');
+    $rootScope.appController.closeMenu();
+
+    url === 'logout' ? Auth.logout() : $(e.delegateTarget).addClass('active');
+
+    // post menu activation/close goes here
+  };
+
+  $rootScope.appController = this;
+}]);
+
+
+
+// just preparing, making sure we don't forget stuff
+app.config(function($sceDelegateProvider, AuthProvider) {
+  AuthProvider.config({
+    authUrl: 'api/login',
+    redirect: true,
+    redicrectUrl: '/login',
+    redirectOnStatus: [412]
+  });
+
+  $sceDelegateProvider.resourceUrlWhitelist([
+    'self',
+    'http://pifa*.herokuapp.org/**'
+  ]);
+});
+
+
+
+// we're going to intercept request and look out for anything suspicious
+// meaning: we're going to look for property 'notify' and call `iPNotify`
+// that's pretty much it --- talk about overkill :)
+app.config(function ($routeProvider, $locationProvider, $logProvider, AuthProvider) {
+  $routeProvider
+
+  .when('/login', {
+    templateUrl: 'views/login.html',
+    controller: 'loginController'
+  })
+
+  .when('/', {
+    templateUrl: 'views/home.html',
+    controller: 'homeController',
+    resolve: {
+      loggedIn: AuthProvider.isLoggedIn
+    }
+  })
+
+  .otherwise({
+    redirectTo: '/login'
+  });
+
+  $locationProvider.html5Mode(true);
+  $logProvider.debugEnabled(true);
+});
---
+++ public/assets/js/controllers/login.js
@@ -0,0 +1,64 @@
+var loginController = app.controller('loginController', ['$scope', '$http', '$location', '$log', '$mdToast', '$timeout', 'Auth', function ($scope, $http, $location, $log, $mdToast, $timeout, Auth) {
+  $scope.credentials = {
+    ID: '',
+    password: ''
+  };
+
+  $scope.newPlayer = {
+    player_username: '',
+    player_password: '',
+    player_email: ''
+  };
+
+  $http.get('api/login').success(function (data, status, headers, config) {
+    $location.path('/').replace();
+  });
+
+  this.login = function () {
+    Auth.login($scope.credentials).then(function (data) {
+      $location.path('/').replace();
+    }, function (data) {
+      $timeout(function() {
+        $mdToast.show({
+          template: '<md-toast style="position: fixed;"><span flex>UNAUTHORIZED Mitch!</span></md-toast>',
+          hideDelay: 3000,
+          position: 'bottom left'
+        });
+      }, 64);
+    });
+  };
+
+  this.singup = function () {
+    $http.post('api/signup', $scope.newPlayer)
+      .success(function (data, status, headers, config) {
+        $timeout(function() {
+          $mdToast.show({
+            template: '<md-toast style="position: fixed;"><span flex>you\'ll be notified by email once you\'re approved</span></md-toast>',
+            hideDelay: 5000,
+            position: 'bottom left'
+          });
+        }, 64);
+      })
+      .error(function (data, status, headers, config) {
+        var toast = '';
+
+        if (status === 409) {
+          toast = '<md-toast style="position: fixed;"><span flex>username or email taken, try a different one</span></md-toast>';
+        } else if (status === 403) {
+          toast = '<md-toast style="position: fixed;"><span flex>one too many requests Mitch!</span></md-toast>'
+        } else {
+          $log.error(data);
+        }
+
+        $timeout(function() {
+          $mdToast.show({
+            template: toast,
+            hideDelay: 3000,
+            position: 'bottom left'
+          });
+        }, 64);
+      });
+  };
+
+  $scope.loginController = this;
+}]);
