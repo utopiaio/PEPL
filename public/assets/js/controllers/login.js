51cbc1342de346ba6db84bc1fc22169318e2896d
Moe Szyslak <moe.duffdude@gmail.com>
2014-12-12 11:52:54 +0300

    login
    - now controller with strict mode
    - redirecting to /

--- public/assets/js/controllers/login.js
+++ public/assets/js/controllers/login.js
@@ -1,44 +1,59 @@
-var loginController = app.controller('loginController', ['$scope', '$http', '$location', 'Toast', 'Auth', function ($scope, $http, $location, Toast, Auth) {
-  $scope.credentials = {
-    ID: '',
-    password: ''
-  };
+(function () {
+  'use strict';
 
-  $scope.newPlayer = {
-    player_username: '',
-    player_password: '',
-    player_email: ''
-  };
+  var loginController = app.controller('loginController', ['$scope', '$http', '$location', 'Toast', 'Auth', function ($scope, $http, $location, Toast, Auth) {
+    $scope.credentials = {
+      ID: '',
+      password: ''
+    };
 
-  $http.get('api/login').success(function (data, status, headers, config) {
-    $location.path('/').replace();
-  });
+    $scope.newPlayer = {
+      player_username: '',
+      player_password: '',
+      player_email: ''
+    };
 
-  this.login = function () {
-    Auth.login($scope.credentials).then(function (data) {
+    $http.get('api/login').success(function (data, status, headers, config) {
       $location.path('/').replace();
-    }, function (data) {
-      Toast.show({template: '<md-toast><span flex>UNAUTHORIZED Mitch!</span></md-toast>'});
     });
-  };
 
-  this.singup = function () {
-    $http.post('api/signup', $scope.newPlayer)
-      .success(function (data, status, headers, config) {
-        Toast.show({template: '<md-toast><span flex>you\'ll be notified by email once you\'re approved</span></md-toast>'});
-      })
-      .error(function (data, status, headers, config) {
-        var toast = '';
-
-        if (status === 409) {
-          toast = '<md-toast><span flex>username or email taken, try a different one</span></md-toast>';
-        } else if (status === 403) {
-          toast = '<md-toast><span flex>one too many requests Mitch!</span></md-toast>'
+    this.login = function () {
+      Auth.login($scope.credentials).then(function (data) {
+        $location.path('/').replace();
+      }, function (status) {
+        if (status === 406) {
+          var toast = '<md-toast><span flex>check yo-self!</span></md-toast>';
+        } else {
+          var toast = '<md-toast><span flex>UNAUTHORIZED Mitch!</span></md-toast>';
         }
 
         Toast.show({template: toast});
       });
-  };
+    };
 
-  $scope.loginController = this;
-}]);
+    this.singup = function () {
+      $http.post('api/signup', $scope.newPlayer)
+        .success(function (data, status, headers, config) {
+          Toast.show({template: '<md-toast><span flex>you\'ll be notified by email once you\'re approved</span></md-toast>'});
+        })
+        .error(function (data, status, headers, config) {
+          var toast = '';
+
+          if (status === 400) {
+            toast = '<md-toast><span flex>that was just BAD!</span></md-toast>';
+          } else if (status === 403) {
+            toast = '<md-toast><span flex>one too many requests Mitch!</span></md-toast>';
+          } else if (status === 406) {
+            toast = '<md-toast><span flex>check yo-self!</span></md-toast>';
+          } else if (status === 409) {
+            toast = '<md-toast><span flex>username or email taken, try a different one</span></md-toast>';
+          }
+
+          Toast.show({template: toast});
+        });
+    };
+
+    $scope.loginController = this;
+  }]);
+
+})();
