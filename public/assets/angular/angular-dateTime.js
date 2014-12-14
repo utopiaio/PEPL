fdd0fc02cb71b3e4cef8c039ada0e0cc5c045d01
Moe Szyslak <moe.duffdude@gmail.com>
2014-12-14 18:21:54 +0300

    there's a reason this branch is called retreat
    - Glyphicons removed from bootstrap, using ionicons
    - bootstrap js returns

--- public/assets/angular/angular-dateTime.js
+++ public/assets/angular/angular-dateTime.js
@@ -2,7 +2,14 @@ angular.module('moeDateTime', []).directive('dateTime', function () {
   return {
     restrict: 'A',
     link: function (scope, element, attribute) {
-      $(element).datetimepicker();
+      $(element).datetimepicker({
+        icons: {
+            time: 'ion-ios7-time-outline',
+            date: 'ion-ios7-calendar-outline',
+            up:   'ion-ios7-arrow-up',
+            down: 'ion-ios7-arrow-down'
+        }
+      });
     }
   };
 });
