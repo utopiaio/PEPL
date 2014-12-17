454df9062e63115e25edd17f9aec1cf34c72deb3
Moe Szyslak <moe.duffdude@gmail.com>
2014-12-17 09:07:55 +0300

    new heroku push

--- app.js
+++ app.js
@@ -32,15 +32,15 @@ var wall = require('./routers/wall');
 // sockets is where we're going to keep all those sockets that are connected
 // {username: socket}
 var sockets = {};
-var cookieSignature = 'COOKIE-SIGNATURE';
+var cookieSignature = 'Mvi#isdf!93O4{5msVldx!fks(8MégåńF0x';
 var emailConfig = {
-  service: 'PLATFORM',
+  service: 'Gmail',
   auth: {
-    user: 'EMAIL',
-    pass: 'PASSWORD'
+    user: 'moe.duffdude@gmail.com',
+    pass: 'juqdrrdzozzcllqy'
   },
-  from: 'ADMIN-EMAIL',
-  adminEmail: 'ADMIN-EMAIL'
+  from: 'Mamoe <moe.duffdude@gmail.com>',
+  adminEmail: 'moe.duffdude@gmail.com'
 };
 
 var emailTransporter = nodemailer.createTransport({
