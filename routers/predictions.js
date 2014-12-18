9c60b5668f8de095257c49288aa002e3d79a8524
Moe Szyslak <moe.duffdude@gmail.com>
2014-12-18 16:53:49 +0300

    prediction fix

--- routers/predictions.js
+++ routers/predictions.js
@@ -61,9 +61,20 @@ module.exports = function (dependency) {
                           }
                         }
 
-                        if (moment(predictions[iPredictions].prediction_timestamp).add(30, 'minutes').isAfter(moment(predictions[iPredictions].prediction_fixture.fixture_time)) || predictions[iPredictions].prediction_player === request.session.player_id) {
+                        // instead of playing with the time, there's a simpler approach
+                        // ONLY fixtures having >-1 are included
+                        if (predictions[iPredictions].prediction_fixture.fixture_team_home_score > -1) {
                           coolPredictions.push(predictions[iPredictions]);
                         }
+
+                        /**
+                         * previous algorithm
+                         * PS
+                         * here, there's Timezone issues which i haven't figured out
+                        if (moment(predictions[iPredictions].prediction_timestamp).add(30, 'minutes').isAfter(moment(predictions[iPredictions].prediction_fixture.fixture_time)) || predictions[iPredictions].prediction_player.player_id === request.session.player_id) {
+                          coolPredictions.push(predictions[iPredictions]);
+                        }
+                        */
                       }
 
                       response.status(200);
