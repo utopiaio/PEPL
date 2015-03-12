432013d3213f26862cdb159abb5d8179f063e725
Moe Szyslak <moe.duffdude@gmail.com>
2015-03-12 09:39:20 +0300

    safety lock

--- public/assets/js/controllers/fixtures.js
+++ public/assets/js/controllers/fixtures.js
@@ -50,9 +50,10 @@
             data[key].age = moment(data[key].fixture_time).fromNow();
             // this accounts for different timezones which isSame "doesn't"
             // games starting at midnight will no longer be on lock-down
-            var dMinus36 = moment(data[key].fixture_time).subtract(300, 'hours'),
-                dPlus36 = moment(data[key].fixture_time).add(300, 'hours');
-            data[key].showInToday = moment().isAfter(dMinus36) && moment().isBefore(dPlus36);
+            var dMinus36 = moment(data[key].fixture_time).subtract(36, 'hours'),
+                dPlus36 = moment(data[key].fixture_time).add(36, 'hours');
+            // data[key].showInToday = moment().isAfter(dMinus36) && moment().isBefore(dPlus36);
+            data[key].showInToday = (moment().isAfter(dMinus36) && moment().isBefore(dPlus36)) || data[key].fixture_id === 163;
             /**
              * 45 minutes - first half
              * 1 minute - minimum stoppage time
