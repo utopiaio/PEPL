e6086f4f2931c8a9c4686253380f82017e12d73a
Moe Szyslak <moe.duffdude@gmail.com>
2015-08-09 08:25:20 +0300

    digest...
    - bootstrap moved to new-fixture so it doesn't mess-up the material
    styling
    - digest applied

--- public/assets/angular/angular-dateTime.js
+++ public/assets/angular/angular-dateTime.js
@@ -5,14 +5,21 @@
     .directive('dateTime', function() {
       return {
         restrict: 'A',
-        link: function(scope, element, attribute) {
-          $(element).datetimepicker({
+        require: 'ngModel',
+        link: function(scope, element, attribute, ngModel) {
+          var datePicker = $(element).datetimepicker({
             icons: {
                 time: 'ion-ios-time-outline',
                 date: 'ion-ios-calendar-outline',
                 up:   'ion-ios-arrow-up',
                 down: 'ion-ios-arrow-down'
             }
+          });
+
+          $(datePicker).on('dp.change', function(date, oldDate) {
+            scope.$apply(function() {
+              ngModel.$setViewValue(date.target.value);
+            });
           });
         }
       };
